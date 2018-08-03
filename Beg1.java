5f8e3b0b04c1e41d7b027c6d1cbc0e8d2f7aacff
Ramadhuvishan <42070850+Ramadhuvishan@users.noreply.github.com>
2018-08-03 21:20:55 +0530

    Update Beg1.java

--- Beg1.java
+++ Beg1.java
@@ -6,12 +6,19 @@ class Main
 {
     public static void main(String args[])
     {
-        int no = sc.nextInt();
-        if(no>0)
-            System.out.println("Positive");
-        else if(no<0)
-            System.out.println("Negative");
-        else
-            System.out.println("Zero");
+        try
+        {
+            int no = sc.nextInt();
+            if(no>0)
+                System.out.println("Positive");
+            else if(no<0)
+                System.out.println("Negative");
+            else
+                System.out.println("Zero");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 }
